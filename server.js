70ab0bb73919daf217b28d975a7e34791b0696cb
Xanthium7 <akshhaykmurali@gmail.com>
2024-11-08 23:08:25 +0530

    srver lowkey good

--- server.js
+++ server.js
@@ -1,13 +1,56 @@
+// server/server.js
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
-  /* options */
+  cors: {
+    origin: "*", // Allow all origins (adjust in production)
+  },
 });
+
+let players = {};
 
 io.on("connection", (socket) => {
-  // ...
+  console.log(`Player connected: ${socket.id}`);
+
+  // Add new player to the players object
+  players[socket.id] = {
+    x: 25,
+    y: 20,
+  };
+
+  // Send the current players to the new player
+  socket.emit("currentPlayers", players);
+
+  // Notify existing players of the new player
+  socket.broadcast.emit("newPlayer", {
+    id: socket.id,
+    x: players[socket.id].x,
+    y: players[socket.id].y,
+  });
+
+  // Listen for player movement
+  socket.on("playerMovement", (movementData) => {
+    players[socket.id] = movementData;
+    // Broadcast the movement to other players
+    socket.broadcast.emit("playerMoved", {
+      id: socket.id,
+      x: movementData.x,
+      y: movementData.y,
+    });
+  });
+
+  // Handle player disconnect
+  socket.on("disconnect", () => {
+    console.log(`Player disconnected: ${socket.id}`);
+    // Remove from players object
+    delete players[socket.id];
+    // Notify remaining players
+    io.emit("playerDisconnected", socket.id);
+  });
 });
 
-httpServer.listen(3000);
+httpServer.listen(3001, () => {
+  console.log("Server is listening on port 3001");
+});
