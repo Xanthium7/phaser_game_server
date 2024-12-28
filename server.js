003a6cfd4a52d60709a87e58977bfcc8723e4744
Xanthium7 <akshhaykmurali@gmail.com>
2024-12-28 19:09:30 +0530

    change default server port from 3001 to 8080

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 httpServer.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
